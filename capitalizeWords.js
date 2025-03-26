6784ce800f5206622c15fe7b71985e20ce0555bc
Timite54 <101226879+Timite54@users.noreply.github.com>
2025-03-26 07:47:28 +0100

    Update capitalizeWords.js

--- capitalizeWords.js
+++ capitalizeWords.js
@@ -1,3 +1,6 @@
 function mettreMotsEnMajuscules(phrase) {
     return phrase.split(' ').map(mot => mot.charAt(0).toUpperCase() + mot.slice(1)).join(' ');
 }
+
+// console.log(mettreMotsEnMajuscules("bonjour tout le monde")); // "Bonjour Tout Le Monde"
+
