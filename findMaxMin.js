31a92dc9f88887c108a9453c6f4dc6b3a112ba0b
Timite54 <101226879+Timite54@users.noreply.github.com>
2025-03-26 07:52:03 +0100

    Update findMaxMin.js

--- findMaxMin.js
+++ findMaxMin.js
@@ -1,5 +1,5 @@
-function trouverMaximum(tableau) {
-    return Math.max(...tableau);
+function trouverMaximum(tab) {
+    return [Math.min(...tab), Math.max(...tab)];
 }
 
-// console.log(trouverMaximum([1, 2, 3, 4, 5])); // 5
+// console.log(trouverMaximum([1, 2, 3, 4, 5])); // [1, 5]
